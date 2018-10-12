1b000d9337bb1b5382c9ce92ebbe019a5392f1eb
Sujin Lee <sujinlee.me@gmail.com>
2018-10-12 18:10:24 +0900

    clear

--- bin/rasterize.js
+++ bin/rasterize.js
@@ -1,27 +1,32 @@
 "use strict";
-const page = require('webpage').create(),
-    system = require('system'),
-    address, output, zoom;
+var page = require("webpage").create(),
+  system = require("system"),
+  address,
+  output,
+  zoom;
 
 address = system.args[1];
 output = system.args[2];
 zoom = system.args[3];
 
 page.paperSize = {
-  format: 'A4',
-  orientation: 'portrait',
-  margin: '0cm'
+  format: "A4",
+  orientation: "portrait",
+  margin: "0cm"
 };
 
-page.open(address, function (status) {
-    if (status !== 'success') {
-        console.log('Unable to load the address!');
-        phantom.exit(1);
-    } else {
-        page.evaluate((zoom) => document.querySelector('body').style.zoom = zoom, zoom);
-        window.setTimeout(function () {
-            page.render(output);
-            phantom.exit();
-        }, 200);
-    }
+page.open(address, function(status) {
+  if (status !== "success") {
+    console.log("Unable to load the address!");
+    phantom.exit(1);
+  } else {
+    page.evaluate(function(zoom) {
+      return (document.querySelector("body").style.zoom = zoom);
+    }, zoom);
+
+    window.setTimeout(function() {
+      page.render(output);
+      phantom.exit();
+    }, 200);
+  }
 });
